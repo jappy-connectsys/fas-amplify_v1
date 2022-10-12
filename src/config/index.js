e5b14797699ffb3aa1fdea3fcfa4804f92acc738
Jap <xiaver99z1@gmail.com>
2022-10-13 05:12:12 +0800

    with build and npm i 10

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const REACT_APP_DIRECTUS_URL =  process.env.REACT_APP_DIRECTUS_URL || `ht
 
 export const REACT_APP_DIRECTUS_TOKEN = process.env.REACT_APP_DIRECTUS_TOKEN || `@spiderman03`
 
-export const COMPANY_ROLE = process.env.COMPANY_ROLE ||  `69EB398A-E245-4DC0-9CE6-6A209EE5EDF9`
+export const COMPANY_ROLE = process.env.REACT_APP_COMPANY_ROLE || `69EB398A-E245-4DC0-9CE6-6A209EE5EDF9`
 
 export const REACT_APP_SENDGRID_LINK_TO_SEND_FORGOT_PASSWORD = process.env.REACT_APP_SENDGRID_LINK_TO_SEND_FORGOT_PASSWORD || `https://moandbear-cms-dev.5i9kftpno7oc0.ap-southeast-1.cs.amazonlightsail.com/flows/trigger/D60F438A-3C5B-4D49-B563-03594A309915`
 
