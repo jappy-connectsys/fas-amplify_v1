82c4652532208655a36e8f0abe5052b5bbc4dbf5
Jap <xiaver99z1@gmail.com>
2022-10-13 04:59:32 +0800

    with build and npm i 8

--- src/config/index.js
+++ src/config/index.js
@@ -4,11 +4,11 @@ export const REACT_APP_DIRECTUS_TOKEN = process.env.REACT_APP_DIRECTUS_TOKEN || 
 
 export const COMPANY_ROLE = process.env.COMPANY_ROLE ||  `69EB398A-E245-4DC0-9CE6-6A209EE5EDF9`
 
-export const SENDGRID_LINK_TO_SEND_FORGOT_PASSWORD = process.env.SENDGRID_LINK_TO_SEND_FORGOT_PASSWORD || `https://moandbear-cms-dev.5i9kftpno7oc0.ap-southeast-1.cs.amazonlightsail.com/flows/trigger/D60F438A-3C5B-4D49-B563-03594A309915`
+export const REACT_APP_SENDGRID_LINK_TO_SEND_FORGOT_PASSWORD = process.env.REACT_APP_SENDGRID_LINK_TO_SEND_FORGOT_PASSWORD || `https://moandbear-cms-dev.5i9kftpno7oc0.ap-southeast-1.cs.amazonlightsail.com/flows/trigger/D60F438A-3C5B-4D49-B563-03594A309915`
 
-export const SENDGRID_LINK = process.env.SENDGRID_LINK || `https://faswebdev.5i9kftpno7oc0.ap-southeast-1.cs.amazonlightsail.com/reset-password`
+export const REACT_APP_SENDGRID_LINK = process.env.REACT_APP_SENDGRID_LINK || `https://main.d1inuk55s9owf8.amplifyapp.com/reset-password`
 
-export const SENDGRID_EMAIL_SENDER = process.env.SENDGRID_EMAIL_SENDER || `john.serafico@connectsys.tech`
+export const REACT_APP_SENDGRID_EMAIL_SENDER = process.env.REACT_APP_SENDGRID_EMAIL_SENDER || `john.serafico@connectsys.tech`
 
-export const SENDGRID_SENDER_NAME = process.env.SENDGRID_SENDER_NAME || `Administrator`
+export const REACT_APP_SENDGRID_SENDER_NAME = process.env.REACT_APP_SENDGRID_SENDER_NAME || `Administrator`
 
